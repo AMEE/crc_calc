76f8fc5d4a1a248811c10a395fee5d806827902e
Andrew Hill <andrew.hill@amee.com>
2010-06-23 15:41:07 +0100

    Added optional hoptoad support - MA-221.

--- config/environment.rb
+++ config/environment.rb
@@ -23,6 +23,7 @@ Rails::Initializer.run do |config|
   config.gem "amee", :lib => "amee", :source => "http://gemcutter.org", :version => '>= 2.0.28'  
   config.gem "prawn", :version => "0.6.3"
   config.gem "prawn-layout", :lib => "prawn/layout", :version => "0.3.2"
+  config.gem 'hoptoad_notifier'
   
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
